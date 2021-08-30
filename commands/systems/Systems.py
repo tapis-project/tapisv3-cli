8871f38e066691c773394e98d37d2be51c6d85db
Nathan Freeman <ndf485@MacBook-Pro.local>
2021-08-30 08:44:59 -0500

    Refactor systems

--- commands/systems/Systems.py
+++ commands/systems/Systems.py
@@ -1,5 +1,6 @@
 from core.Command import Command
 from tapipy.errors import InvalidInputError
+import json, sys
 
 class Systems(Command):
     def __init__(self):
@@ -11,6 +12,29 @@ class Systems(Command):
             "--test": []
         })
 
+    def create(client, system_definition_file) -> None:
+        system_definition = json.loads(open(system_definition_file, "r").read())
+        client.systems.createSystem(**system_definition)
+
+        return
+
+    def delete(client, system_id) -> None:
+        try:
+            client.systems.deleteSystem(systemId=system_id)
+            print(f"Deleted system with id '{system_id}'")
+            return
+        except InvalidInputError:
+            print(f"System not found with id '{system_id}'")
+            return
+
+    def get(self, client, system_id) -> None:
+        try:
+            system = client.systems.getSystem(systemId=system_id)
+            print(system)
+            return
+        except InvalidInputError:
+            print(f"Sytem not found with id '{system_id}'")
+
     def list(self, client) -> None:
         systems = client.systems.getSystems()
         if len(systems) > 0:
@@ -21,10 +45,24 @@ class Systems(Command):
         print(f"No systems found for user 'xxx'")
         return
 
-    def get(self, client, system_id) -> None:
+    def undelete(client, system_id) -> None:
         try:
-            system = client.systems.getSystem(systemId=system_id)
-            print(system)
+            client.systems.undeleteSystem(systemId=system_id)
+            print(f"Recovered system with id '{system_id}'")
             return
         except InvalidInputError:
-            print(f"Sytem not found with id '{system_id}'")
+            print(f"Deleted system not found with id '{system_id}'")
+            return
+
+    def update(client, system_definition_file) -> None:
+        system_definition = json.loads(open(system_definition_file, "r").read())
+
+        try:
+            # Update select attributes defined by the system definition file.
+            client.systems.patchSystem(**system_definition)
+            return
+        except InvalidInputError as e:
+            print(f"Invalid Input Error: '{e.message}'")
+        except:
+            e = sys.exc_info()[0]
+            print( f"Error: {e}" )

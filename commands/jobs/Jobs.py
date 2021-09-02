242b1a2391ae6c91fc259a6386258745da2bfe93
Nathan Freeman <nathan.dean.freeman@gmail.com>
2021-09-02 08:49:57 -0500

    Add health and ready check function on TapisCommand. Overload help method in TapisCommand and list all available tapipy methods

--- commands/jobs/Jobs.py
+++ commands/jobs/Jobs.py
@@ -11,21 +11,53 @@ class Jobs(TapisCommand):
             "--test": []
         })
 
-    def get(self, client, id) -> None:
+    def download(self, client, uuid, output_path) -> None:
         try:
-            job = client.jobs.getJob(jobId=id)
-            print(job)
+            client.jobs.getJobOutputDownload(jobUuid=uuid, outputPath=output_path)
+            self.logger.complete(f"Download complete for job {uuid}")
+            return
+        except InvalidInputError as e:
+            self.logger.error(f"{e.message}")
+
+    def get(self, client, uuid) -> None:
+        try:
+            job = client.jobs.getJob(jobUuid=uuid)
+            self.logger.log(job)
             return
         except InvalidInputError:
-            print(f"Job not found with id '{id}'")
+            self.logger.error(f"Job not found with uuid '{uuid}'")
+
+    def history(self, client, uuid) -> None:
+        try:
+            status = client.jobs.getJobHistory(jobUuid=uuid)
+            self.logger.log(status)
+
+            return
+        except InvalidInputError:
+            self.logger.error(f"Job not found with uuid '{uuid}'")
+            self.exit(1)
+
+    def status(self, client, uuid) -> None:
+        try:
+            status = client.jobs.getJobStatus(jobUuid=uuid)
+            self.logger.log(status)
+
+            return
+        except InvalidInputError:
+            self.logger.error(f"Job not found with uuid '{uuid}'")
+            self.exit(1)
 
     def list(self, client) -> None:
         jobs = client.jobs.getJobList()
-        print(jobs)
+        self.logger.log(jobs)
 
-    def submit(self, client, name, id, version) -> None:
+    # NOTE the requirement on description is a bug in the api. Leave
+    # description as required for now
+    def submit(self, client, name, app_id, app_version, description) -> None:
         try:
-            client.jobs.submitJob(name=name, appId=id, appVersion=version)
+            job = client.jobs.submitJob(name=name, appId=app_id, appVersion=app_version, description=description)
+            self.logger.info(f"Job submitted. Uuid: {job.uuid}")
             return
         except InvalidInputError as e:
-            print(e.message)
+            self.logger.error(e.message)
+            self.exit(1)
--- core/TapisCommand.py
+++ core/TapisCommand.py
@@ -5,8 +5,7 @@ class TapisCommand(Command):
     def __init__(self):
         Command.__init__(self)
 
-    # TODO Remove
-    def dirs(self, client):
+    def help(self, client):
         all_methods = dir(getattr(client, type(self).__name__.lower()))
         methods = []
 
@@ -16,3 +15,43 @@ class TapisCommand(Command):
                 methods.append(method)
 
         self.logger.log(methods)
+
+    def checkhealth(self, client):
+        command_name = type(self).__name__
+        command = getattr(client, command_name.lower())
+        if hasattr(command, "checkHealth"):
+            self.logger.log(command.checkHealth())
+            return
+
+        self.logger.error(f"Command {type(self).__name__} has no action 'checkHealth'")
+        self.exit(1)
+
+    def healthcheck(self, client):
+        command_name = type(self).__name__
+        command = getattr(client, command_name.lower())
+        if hasattr(command, "healthCheck"):
+            self.logger.log(command.healthCheck())
+            return
+
+        self.logger.error(f"Command {type(self).__name__} has no action 'healthCheck'")
+        self.exit(1)
+
+    def readycheck(self, client):
+        command_name = type(self).__name__
+        command = getattr(client, command_name.lower())
+        if hasattr(command, "readyCheck"):
+            self.logger.log(command.readyCheck())
+            return
+
+        self.logger.error(f"Command {type(self).__name__} has no action 'readyCheck'")
+        self.exit(1)
+
+    def ready(self, client):
+        command_name = type(self).__name__
+        command = getattr(client, command_name.lower())
+        if hasattr(command, "ready"):
+            self.logger.log(command.ready())
+            return
+
+        self.logger.error(f"Command {type(self).__name__} has no action 'ready'")
+        self.exit(1)

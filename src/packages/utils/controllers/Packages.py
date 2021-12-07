c6fb17158554c4ccbecfe36674b8ec625cebd621
Nathan Freeman <nathan.dean.freeman@gmail.com>
2021-12-07 17:51:58 -0600

    Register package on create

--- src/packages/utils/controllers/Packages.py
+++ src/packages/utils/controllers/Packages.py
@@ -19,11 +19,8 @@ class Packages(BaseController):
     def index(self):
         self.list()
 
-    def create(self, *args):
-        if len(args) > 1:
-            package_name = args[0]
-        else:
-            package_name = prompt.text("Package name")
+    def create(self):
+        package_name = prompt.text("Package name")
 
         package_dir = PACKAGES_DIR + package_name + "/"
         os.mkdir(package_dir)
@@ -34,7 +31,8 @@ class Packages(BaseController):
         copyfile(f"{TEMPLATES_DIR}controllers/Configure.py", package_dir + "controllers/Configure.py")
 
         self.logger.complete(f"Package '{package_name}' created.")
-        self.logger.info(f"Don't forget to add '{package_name}' to the PACKAGES array src/conf/settings.py")
+        self.configure(package_name=package_name)
+        self.logger.info(f"Don't forget to add '{package_name}' to the PACKAGES array in src/conf/settings.py")
 
     def list(self):
         nl = "\n"
@@ -42,18 +40,21 @@ class Packages(BaseController):
         self.logger.log(f"Packages:{nl}- {join_str.join(PACKAGES)}")
 
     def use(self):
-        current_package = config.get("current", "package")
+        current_package = self.config.get("current", "package")
         string = f"[{current_package}]"
         answer = prompt.select(
             f"Choose a package {s.muted(string)}",
             PACKAGES,
             sort=True
         )
-        config.add("current", "package", answer)
+        self.config.add("current", "package", answer)
         self.logger.complete(f"Using package '{answer}'")
 
-    def configure(self):
-        package = prompt.select("Choose a package to configure", PACKAGES, sort=True)
+    def configure(self, package_name=None):
+        package = package_name
+        if package_name == None:
+            package = prompt.select("Choose a package to configure", PACKAGES, sort=True)
+        
         configure_ns = f"packages.{package}.controllers.Configure"
         has_configure = bool(find_spec(configure_ns))
         if has_configure == False:
@@ -65,8 +66,24 @@ class Packages(BaseController):
             self.logger.warn(f"Package '{package}' has no category 'Configure'")
             return
 
+        # Temporarily switch current package to configure this package
+        original_package = self.config.get("current", "package")
+        self.config.add("current", "package", package)
+
         configure_controller = getattr(module, "Configure")()
         configure_controller.index()
+
+        # Prompt use to switch to newly configured package
+        if original_package != package:
+            switch_package = prompt.select(
+                f"Switch current package to '{package}'",
+                ["yes", "no"]
+            )
+
+            # Swtich current package back to the original package
+            # if they chooses not to switch
+            if switch_package == "no":
+                self.config.add("current", "package", original_package)
 
         self.logger.complete(f"Package '{package}' configured")
 

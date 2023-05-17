fd1a9cb21c88403f7fb367a7dd86f7b6f6cfe06d
Nathan Freeman <nathan.dean.freeman@gmail.com>
2023-05-17 17:19:23 -0500

    add tapis shell

--- src/main.py
+++ src/main.py
@@ -4,14 +4,28 @@ import sys
 from core.Router import Router
 from utils.Logger import logger
 
-
-def main():
-    """Resolve the category, command, options, and arguments, and then execute them."""
-    (controller, args) = Router().resolve(sys.argv[1:])
+def run(args):
+    (controller, args) = Router().resolve(args)
     try:
         controller.invoke(args)
     except Exception as e:
         logger.error(e)
+
+def main():
+    if len(sys.argv[1:]) > 0 and sys.argv[1:][0] == "shell":
+        try:
+            while True:
+                string = input("tapis> ")
+                args = string.split(" ") if string != "" else []
+                run(args)
+        except KeyboardInterrupt:
+            print("Closing tapis shell")
+            return
+        except SystemExit:
+            main()
+            return
+
+    run(sys.argv[1:])
 
 
 if __name__ == "__main__":

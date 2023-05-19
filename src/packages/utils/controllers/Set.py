c50920bacbafb7af5a98a528995a76e933dcf25e
Nathan Freeman <nathan.dean.freeman@gmail.com>
2023-05-19 13:00:54 -0500

    allow user to update username and jwt

--- src/packages/utils/controllers/Set.py
+++ src/packages/utils/controllers/Set.py
@@ -10,6 +10,43 @@ class Set(BaseController):
     def __init__(self):
         BaseController.__init__(self)
 
+    def username(self):
+        # Prompt for the new username
+        current_user = config_manager.get_current_user()
+        new_username = prompt.text("New username", default=current_user, required=True)
+        
+        # Create the modified profile
+        current_profile = config_manager.get_profile(current_user)
+        modified_profile = {**current_profile, "username": new_username}
+
+        # Update the current user
+        config_manager.set_current_user(new_username)
+
+        # Update the profile list with the new profile
+        modified_profiles = []
+        config = config_manager.load()
+        for profile in config.get("profiles"):
+            # Add only non-current user profiles to the list
+            if profile["username"] != current_user:
+                modified_profiles.append(profile)
+
+        # Add the newly modified profile to the profiles list
+        modified_profiles.append(modified_profile)
+        config["profiles"] = modified_profiles
+        
+        config_manager.write(config)
+
+        self.logger.complete(f"Username updated from '{current_user}' to '{new_username}'")
+
+    def jwt(self):
+        current_user = config_manager.get_current_user()
+        jwt = prompt.text("New JWT", required=True)
+
+        config_manager.update_profile(current_user, jwt=jwt)
+
+        self.logger.complete(f"JWT updated")
+        
+
     def outputType(self):
         config = config_manager.load()
         config["output_type"] = prompt.select("Set output type", [enum.value for enum in OutputEnum])

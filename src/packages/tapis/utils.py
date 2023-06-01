9eb1ee625d7a93743b5da6f4c2640fd34f2c1081
Nathan Freeman <nathan.dean.freeman@gmail.com>
2023-06-01 17:39:41 -0500

    Catch edgecase for json response returned from tapipy

--- src/packages/tapis/utils.py
+++ src/packages/tapis/utils.py
@@ -1,5 +1,11 @@
 def serialize_result(result):
         if type(result) in [str, int, float, type(None), bool]: return result
+
+        # NOTE FIXME REMOVE WHEN FIXED IN TAPIPY
+        # Catch an edgecase where tapipy returns the actual JSON response
+        # object and not a TapisResult
+        if type(result) == dict and result.get("message", None) != None:
+            return result["message"]
         
         if type(result) == list:
             modified_result = []

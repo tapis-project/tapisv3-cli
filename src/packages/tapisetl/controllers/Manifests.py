5f437912c85e301e1a80c5aeccb69faf6df67da4
tacc-afields <afields@tacc.utexas.edu>
2024-04-11 16:48:30 -0500

    get file_names_to_track, determine invalid files, create manifest schema, write manifest to path

--- src/packages/tapisetl/controllers/Manifests.py
+++ src/packages/tapisetl/controllers/Manifests.py
@@ -1,5 +1,4 @@
-import json
-import sys
+import json, time, uuid, os
 
 from tapipy.errors import InvalidInputError
 
@@ -13,35 +12,67 @@ class Manifests(ETLController):
     """Contains all of the CRUD functions associated with applications."""
     def __init__(self):
         ETLController.__init__(self)
-        
-    def generate_Action(self, data_system_id, data_path, manifest_system_id) -> None:
-        """Check if a system is currently enabled."""
+      
+    def generate_Action(
+            self,
+            data_system_id: str,
+            data_path: str,
+            manifest_system_id: str,
+            manifest_path: str,
+            *file_names_to_track: tuple[str]
+        ) -> None:
+        # Fetch all of the files on the data system at the data path
         try:
-            system = self.client.systems.isEnabled(systemId=data_system_id)
-            status = "enabled" if system.aBool else "disabled"
-            self.logger.info(f"The system '{data_system_id}' is {status}\n")
-        except InvalidInputError:
-            self.logger.error(f"System not found with id '{data_system_id}'\n")
+            all_data_files = self.client.files.listFiles(systemId=data_system_id, path=data_path)
+        except Exception as e:
+            self.logger.error(f"Error fetching files for '{data_system_id}': {e}\n")
         
-        """Check if data_path is good"""
-        if status == "enabled":
-            try:
-                local_inbox = self.client.files.listFiles(systemId=data_system_id, path=data_path)
-                self.logger.info(f"The files for '{data_system_id}' are: \n") 
-                for file in local_inbox: print(file)
-            except Exception as e:
-                self.logger.error(f"Error fetching files for '{data_system_id}': {e}\n")
+        # Get the file objects that correspond to the names provided in
+        # file_names_to_track
+        data_files = []
+        for file in all_data_files:
+            if file.name in file_names_to_track:
+                data_files.append(file)
+
+        # Determine invalid files
+        invalid_data_files = []
+        data_file_names = [f.name for f in data_files]
+        for file_name_to_track in file_names_to_track:
+            if file_name_to_track not in data_file_names:
+                invalid_data_files.append(file_name_to_track)
+
+        # Raise Exception if user provides invalid file names (names of files that
+        # do not exist)
+        if len(invalid_data_files) > 0:
+            raise Exception(f"Invalid data file name(s) provided: {invalid_data_files}")
+
+        created_at = time.time()
+        new_manifest = {
+            "status": "pending",
+            "phase": "ingress",
+            "local_files": [],
+            "transfers": [],
+            "remote_files": [data_file.__dict__ for data_file in data_files],
+            "jobs": [],
+            "logs": [f"{created_at} Created"],
+            "created_at": created_at,
+            "last_modified": created_at,
+            "metadata": {}
+        }
         
-        """Check if the manifest system is available"""
+        
+        manifest_file_name = f"{str(uuid.uuid4())}.json"
         try:
-            manifest_system = self.client.systems.isEnabled(systemId=manifest_system_id)
-            manifest_status = "enabled" if manifest_system.aBool else "disabled"
-            self.logger.info(f"The system '{manifest_system_id}' is {manifest_status}\n")
-        except InvalidInputError:
-            self.logger.error(f"System not found with id '{manifest_system_id}'\n")
-        # """Check if the local path is available"""
-        
-        
+            new_manifest_path = os.path.join(manifest_path, manifest_file_name)
+            self.client.files.insert(
+                systemId=manifest_system_id,
+                path=new_manifest_path,
+                file=json.dumps(new_manifest)
+            )
+            self.logger.complete(f"Created manifest '{manifest_file_name}' on system '{manifest_system_id}' | Path: {new_manifest_path}")
+        except Exception as e:
+            self.logger.error(f"Error writing: '{file}' to '{new_manifest_path}': {e}")
+       
 """
     def manifestPrompt(self, *_, **__):
         #Prompt user for data ingress and egress
